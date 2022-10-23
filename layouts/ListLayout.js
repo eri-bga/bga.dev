de8ebee06df989f93ed79bd49c4af549ceef5694
bashebr <bgashebr@gmail.com>
2022-10-23 21:58:52 +0200

    update: Additinal layout changes

--- layouts/ListLayout.js
+++ layouts/ListLayout.js
@@ -20,87 +20,83 @@ export default function ListLayout({ posts, title, initialDisplayPosts = [], pag
   return (
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="w-100">
-          <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-            <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-              {title}
-            </h1>
-            <div className="relative mx-auto max-w-lg rounded-full bg-gradient-to-r from-slate-100 via-sky-400 to-indigo-400 p-1">
-              <input
-                aria-label="Search articles"
-                type="text"
-                onChange={(e) => setSearchValue(e.target.value)}
-                placeholder="Search articles"
-                className="w-full rounded-full border border-gray-300 bg-white p-3 px-4 py-2 text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-900 dark:bg-gray-800 dark:text-gray-100"
+        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            {title}
+          </h1>
+          <div className="relative mx-auto max-w-lg rounded-full bg-gradient-to-r from-slate-100 via-sky-400 to-indigo-400 p-1">
+            <input
+              aria-label="Search articles"
+              type="text"
+              onChange={(e) => setSearchValue(e.target.value)}
+              placeholder="Search articles"
+              className="w-full rounded-full border border-gray-300 bg-white p-3 px-4 py-2 text-gray-900 focus:border-primary-500 focus:ring-primary-500 dark:border-gray-900 dark:bg-gray-800 dark:text-gray-100"
+            />
+            <svg
+              className="absolute right-3 top-3 h-5 w-5 text-gray-400 dark:text-gray-300"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
               />
-              <svg
-                className="absolute right-3 top-3 h-5 w-5 text-gray-400 dark:text-gray-300"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                />
-              </svg>
-            </div>
+            </svg>
           </div>
-
-          {!filteredBlogPosts.length && 'No posts found.'}
-          {displayPosts.map((frontMatter) => {
-            const { slug, date, title, summary, tags } = frontMatter
-            return (
-              <div
-                key={slug}
-                className="mb-4 block max-w-3xl border-spacing-6 rounded-lg border bg-gradient-to-r
+        </div>
+        {!filteredBlogPosts.length && 'No posts found.'}
+        {displayPosts.map((frontMatter) => {
+          const { slug, date, title, summary, tags } = frontMatter
+          return (
+            <div
+              key={slug}
+              className="mb-4 block max-w-3xl border-spacing-6 rounded-lg border bg-gradient-to-r
                   from-indigo-100 
                   via-purple-400 to-pink-400 p-1 text-center dark:border-gray-700"
-              >
-                <div className="mx-auto bg-gray-300 hover:bg-cyan-100 dark:bg-gray-800 dark:hover:bg-gray-700">
-                  <article className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
-                    <div className="space-y-3 xl:col-span-3">
-                      <div>
-                        <h3 className="text-2xl font-bold leading-8 tracking-tight">
-                          <Link href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100">
-                            {title}
-                          </Link>
-                        </h3>
-                        <dl>
-                          <dt className="sr-only">Published on</dt>
-                          <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                            <time dateTime={date}>{formatDate(date)}</time>
-                          </dd>
-                        </dl>
-                        <div className="flex flex-wrap justify-center">
-                          {tags.map((tag) => (
-                            <Tag key={tag} text={tag} />
-                          ))}
-                        </div>
-                      </div>
-                      <div className="prose max-w-none text-gray-500 dark:text-gray-400">
-                        {summary}
-                      </div>
-                      <div className="inline-flex items-center rounded-lg bg-blue-700 py-2 px-3 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        <Link
-                          href={`/blog/${slug}`}
-                          className="text-primary-00 pt-1 hover:text-primary-600 dark:hover:text-primary-200"
-                          aria-label={`Read "${title}"`}
-                        >
-                          Read more &rarr;
+            >
+              <div className="mx-auto bg-gray-300 hover:bg-cyan-100 dark:bg-gray-800 dark:hover:bg-gray-700">
+                <article className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+                  <div className="space-y-3 xl:col-span-3">
+                    <div>
+                      <h3 className="text-2xl font-bold leading-8 tracking-tight">
+                        <Link href={`/blog/${slug}`} className="text-gray-900 dark:text-gray-100">
+                          {title}
                         </Link>
+                      </h3>
+                      <dl>
+                        <dt className="sr-only">Published on</dt>
+                        <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                          <time dateTime={date}>{formatDate(date)}</time>
+                        </dd>
+                      </dl>
+                      <div className="flex flex-wrap justify-center">
+                        {tags.map((tag) => (
+                          <Tag key={tag} text={tag} />
+                        ))}
                       </div>
                     </div>
-                  </article>
-                </div>
+                    <div className="prose max-w-none text-gray-500 dark:text-gray-400">
+                      {summary}
+                    </div>
+                    <div className="inline-flex items-center rounded-lg bg-blue-700 py-2 px-3 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                      <Link
+                        href={`/blog/${slug}`}
+                        className="text-primary-00 pt-1 hover:text-primary-600 dark:hover:text-primary-200"
+                        aria-label={`Read "${title}"`}
+                      >
+                        Read more &rarr;
+                      </Link>
+                    </div>
+                  </div>
+                </article>
               </div>
-            )
-          })}
-        </div>
-        <div className="w-64 bg-slate-50"></div>
+            </div>
+          )
+        })}
       </div>
       {pagination && pagination.totalPages > 1 && !searchValue && (
         <Pagination currentPage={pagination.currentPage} totalPages={pagination.totalPages} />
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,9 @@ import { PageSEO } from '@/components/SEO'
 import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { getAllFilesFrontMatter } from '@/lib/mdx'
+import { getAllTags } from '@/lib/tags'
 import formatDate from '@/lib/utils/formatDate'
+import Tags from './tags'
 
 import NewsletterForm from '@/components/NewsletterForm'
 
@@ -11,11 +13,14 @@ const MAX_DISPLAY = 5
 
 export async function getStaticProps() {
   const posts = await getAllFilesFrontMatter('blog')
+  const tags = await getAllTags('blog')
 
-  return { props: { posts } }
+  return {
+    props: { posts, tags },
+  }
 }
 
-export default function Home({ posts }) {
+export default function Home({ posts, tags }) {
   return (
     <>
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
@@ -28,62 +33,67 @@ export default function Home({ posts }) {
             {siteMetadata.header}
           </p>
         </div>
-        <div className="pt-4">
-          {!posts.length && 'No posts found.'}
-          {posts.slice(0, MAX_DISPLAY).map((frontMatter) => {
-            const { slug, date, title, summary, tags } = frontMatter
-            return (
-              <div
-                key={slug}
-                className=" mb-4 block max-w-3xl border-spacing-6 rounded-lg border bg-gradient-to-r from-indigo-100
-                via-purple-400 to-pink-400 p-1 text-center"
-              >
-                <div className="mx-auto bg-gray-300 hover:bg-cyan-100 dark:bg-gray-800 dark:hover:bg-gray-700">
-                  <article>
-                    <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
-                      <div className="space-y-5 xl:col-span-3">
-                        <div className="space-y-6">
-                          <div>
-                            <h2 className="text-2xl font-bold leading-8 tracking-tight">
-                              <Link
-                                href={`/blog/${slug}`}
-                                className="text-cyan-900 dark:text-blue-300"
-                              >
-                                {title}
-                              </Link>
-                            </h2>
-                            <dl>
-                              <dt className="sr-only">Published on</dt>
-                              <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                                <time dateTime={date}>{formatDate(date)}</time>
-                              </dd>
-                            </dl>
-                            <div className="flex flex-wrap justify-center">
-                              {tags.map((tag) => (
-                                <Tag key={tag} text={tag} />
-                              ))}
+        <div className="flex flex-wrap">
+          <div className="flex-auto pt-5">
+            {!posts.length && 'No posts found.'}
+            {posts.slice(0, MAX_DISPLAY).map((frontMatter) => {
+              const { slug, date, title, summary, tags } = frontMatter
+              return (
+                <div
+                  key={slug}
+                  className="mb-4 block max-w-3xl border-spacing-6 rounded-lg border bg-gradient-to-r from-indigo-100
+                  via-purple-400 to-pink-400 p-1 text-center"
+                >
+                  <div className="mx-auto bg-gray-300 hover:bg-cyan-100 dark:bg-gray-800 dark:hover:bg-gray-700">
+                    <article>
+                      <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+                        <div className="space-y-5 xl:col-span-3">
+                          <div className="space-y-6">
+                            <div>
+                              <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                                <Link
+                                  href={`/blog/${slug}`}
+                                  className="text-cyan-900 dark:text-blue-300"
+                                >
+                                  {title}
+                                </Link>
+                              </h2>
+                              <dl>
+                                <dt className="sr-only">Published on</dt>
+                                <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                                  <time dateTime={date}>{formatDate(date)}</time>
+                                </dd>
+                              </dl>
+                              <div className="flex flex-wrap justify-center">
+                                {tags.map((tag) => (
+                                  <Tag key={tag} text={tag} />
+                                ))}
+                              </div>
+                            </div>
+                            <div className="prose max-w-none text-gray-500 dark:text-gray-400">
+                              {summary}
                             </div>
                           </div>
-                          <div className="prose max-w-none text-gray-500 dark:text-gray-400">
-                            {summary}
+                          <div className="inline-flex items-center rounded-lg bg-blue-700 py-2 px-3 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            <Link
+                              href={`/blog/${slug}`}
+                              className="text-primary-00 pt-1 hover:text-primary-600 dark:hover:text-primary-200"
+                              aria-label={`Read "${title}"`}
+                            >
+                              Read more &rarr;
+                            </Link>
                           </div>
                         </div>
-                        <div className="inline-flex items-center rounded-lg bg-blue-700 py-2 px-3 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                          <Link
-                            href={`/blog/${slug}`}
-                            className="text-primary-00 pt-1 hover:text-primary-600 dark:hover:text-primary-200"
-                            aria-label={`Read "${title}"`}
-                          >
-                            Read more &rarr;
-                          </Link>
-                        </div>
                       </div>
-                    </div>
-                  </article>
+                    </article>
+                  </div>
                 </div>
-              </div>
-            )
-          })}
+              )
+            })}
+          </div>
+          <div className="mx-auto justify-center">
+            <Tags tags={tags} />
+          </div>
         </div>
       </div>
       {posts.length > MAX_DISPLAY && (
